f9693de8da3f1fe1692c63b84afdcc679c1d5adc
Sergey <cabyrkih@mail.ru>
2021-02-08 16:27:59 +0300

    slider

--- src/library/swiper/swiper.js
+++ src/library/swiper/swiper.js
@@ -1,9 +1,8 @@
 const swiper = new Swiper('.about__slaider-conteiner', {
 	direction: 'vertical',
 	loop: true,
-
 	pagination: {
-		el: '.swiper-pagination',
+		el: '.about__slaider-pagination',
 		clickble: true,
 	},
 
@@ -11,16 +10,35 @@ const swiper = new Swiper('.about__slaider-conteiner', {
 	nextEl: '.swiper-button-next',
 	prevEl: '.swiper-button-prev',
 	},
-	slidesPerView:2,
+
+	grabCursor: true,
+
+	slidesPerView: 2,
+	freeMode:true,
+	
+	hashNavigation: {
+		watchState:true,
+	},
+	mousewheel:{
+		sensitivity:1,
+		eventsTarget: ".about__slaider-conteiner" 
+	},
 	// effect:'cube',
 	// cubeEffect:  {
 	// 	slideShadows: true,
 	// 	shadow: true,
 	// 	shadowOffset: 20,
-	// 	shadowScale:0.7
+	// 	shadowScale:0.93,
 	// },
+	effect:'coverflow',
+	coverflowEffect: {
+		rotate: 5,
+		stretch:15,
+		slideShadows: false,
+	},
 	spaceBetween: 90,
-	freeModeMomentumBounce:true,
+	freeModeMomentumBounce: true,
+	
 	autoplay: {
 		delay: 2000,
 		stopOnLastSlide: true,
