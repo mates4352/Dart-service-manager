cbf1e85b40b60f73a6080680c02d3f995c55d74b
Sergey <cabyrkih@mail.ru>
2021-03-25 18:45:33 +0300

    js.mask.tel

--- dist/js/app.js
+++ dist/js/app.js
@@ -375,6 +375,12 @@ for (let anchor of anchors) {
 		})
 	})
 }
+const element = document.getElementById('tel');
+const maskOptions = {
+  mask: '+{7}(000)000-00-00'
+};
+const mask = IMask(element, maskOptions);
+
 
 
 
--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,8 @@
 //= components/_contact-bth.js
 //= components/_propress-bar.js
 //= components/_scroll-menu.js
+//= components/_input-mask.js
+
 
 
 //= ../library/swiper/swiper-bundle.min.js
